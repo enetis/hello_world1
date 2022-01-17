04acf598effbbab043b20c1d2f51c3e1784e2625
enetis <enetis@gmail.com>
2022-01-17 11:49:29 -0500

    upload new python files

---
+++ draw.py
@@ -0,0 +1,13 @@
+import turtle
+painter = turtle.Turtle()
+<<<<<<< HEAD
+
+for _ in range(18):
+	painter.forward(100)
+	painter.left(100)
+	
+=======
+painter.forward(100)
+painter.left(90)
+>>>>>>> parent of 5f40b03 (repeat 4 times wit a for loop)
+turtle.done()
